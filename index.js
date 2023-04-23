8252004c39e115047b0a43a5f56a6390dc1a6423
Grigory Vakulsky <g.vakulsky@gmail.com>
2023-04-23 20:48:42 +0300

    Modify for Vercel

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { getSpreadsheetData } = require('./spreadsheet.js');
-
-//Configure app
-dotenv.config();
+const express = require("express");
 const app = express();
+const spreadsheet = require("./spreadsheet");
+
 app.use(express.json());
+
+app.use("/spreadsheet", spreadsheet);
+
 const PORT = process.env.PORT || 3001;
-
-//Process request
-app.post('/spreadsheet', async (req, res) => {
-    const token = req.body.token;
-    const doc_id = req.body.doc_id;
-    const sheet_index = req.body.sheet_index;
-    const cellsRange = req.body.cells_range;
-    const cells = req.body.cells;
-
-  if (token !== process.env.TOKEN) {
-    return res.status(403).send('Invalid token');
-  }
-
-    const data = await getSpreadsheetData(doc_id, sheet_index, cellsRange, cells);
-    res.status(200).json(data);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
